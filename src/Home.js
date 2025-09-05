a7201fa1705a93d91df688751f05e4cc3e35ebee
“Hemant <hem789ant1997@outlook.com>
2025-09-05 12:45:45 +0530

    comments for readability

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,30 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
+
+  //creates a state variable notes(array) to hold all notes, setNotes is a function to update the notes array
+  //react connection uses the useState hook to manage state
   const [notes, setNotes] = useState([]);
   const [note, setNote] = useState({ title: "", content: "" });
 
+  //backend api url
   const API_URL = "https://notes-app-backend-34154ef4c3d1.herokuapp.com/api/notes";
 
+
+  //loads for the first time
   useEffect(() => {
     fetchNotes();
   }, []);
 
+  /*defines an async function to fetch notes from the backend, uses axios to send a GET req to API
+  updates the notes state with the data received from the backend*/
   const fetchNotes = async () => {
     const res = await axios.get(API_URL);
     setNotes(res.data);
   };
 
+
+  //to submit a note
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (note.id) {
@@ -28,13 +38,16 @@ function App() {
     fetchNotes();
   };
 
+  //to edit the current note
   const handleEdit = (n) => setNote(n);
 
+  //to delete a note
   const handleDelete = async (id) => {
     await axios.delete(`${API_URL}/delete/${id}`);
     fetchNotes();
   };
 
+  //to share a note, generates a shareable link for the note, uses the shareToken received from the backend for each note
   const handleShare = (note) => {
   const shareUrl = `${window.location.origin}/view/${note.shareToken}`;
   navigator.clipboard.writeText(shareUrl);
